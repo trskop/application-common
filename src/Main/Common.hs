100ed733063d4383f4d195dc937c9fd65b31c137
Peter Trško <peter.trsko@gmail.com>
2013-06-29 10:19:35 +0200

    Refactoring module Main.Common
    * Version bumped to 0.2.0.0
    * Introducing Parameters data type along with its instance for
      Default.
    * Functions printHelp and printVersion now have new type signatures.
    * New function printOptErrors.

--- src/Main/Common.hs
+++ src/Main/Common.hs
@@ -9,46 +9,127 @@
 -- Portability:  portable
 --
 -- Commonly used functions for creating command line tools.
+--
+-- Module introduced in version @0.1.2.0@.
 module Main.Common
-    ( printHelp
+    ( Parameters(..)
+    , printHelp
     , printVersion
+    , printOptErrors
     )
     where
 
-import Control.Monad (unless)
-import Data.Version (Version, showVersion)
-import System.IO (Handle, hPutStr, hPutStrLn)
+import Control.Monad (unless, when)
+import Data.Version (Version(Version), showVersion)
+import System.IO (Handle, hPutStr, hPutStrLn, stdout)
 
+import Data.Default.Class (Default(def))
+
+
+
+-- | Data type that encapsulates functions for getting necessary information
+-- from configuration.
+--
+-- Introduced in version @0.2.0.0@.
+data Parameters a = Parameters
+    { paramOutputHandle :: a -> Handle
+    -- ^ Handle to which version and usage is printed.
+    , paramProgName :: a -> String
+    -- ^ Program name.
+    , paramCommand :: a -> String
+    -- ^ Most commonly output of @System.Environment.getProgName@.
+
+    -- Version related:
+
+    , paramVersion :: a -> Version
+    -- ^ Program version number.
+    , paramVersionInfoFooter :: a -> [String]
+    -- ^ Lines printed right after version number.
+
+    -- Help related:
+
+    , paramUsage :: a -> [String]
+    -- ^ Usage lines without program name.
+    , paramHelpInfoFooter :: a -> [String]
+    -- ^ Lines printed after help information.
+    , paramIncludeVersionInfoInHelpInfo :: a -> Bool
+    , paramIncludeVersionFooterInHelpInfo :: a -> Bool
+    }
+
+-- | All functions ignore their argument and produce empty/default values.
+--
+-- > def = Parameters
+-- >     { paramOutputHandle = const stdout
+-- >     , paramProgName = const ""
+-- >     , paramCommand = const ""
+-- >     , paramVersion = const $ Version [] []
+-- >     , paramVersionInfoFooter = const []
+-- >     , paramUsage = const []
+-- >     , paramHelpInfoFooter = const []
+-- >     , paramIncludeVersionInfoInHelpInfo = const True
+-- >     , paramIncludeVersionFooterInHelpInfo = const False
+-- >     }
+--
+-- Instance declaration introduced in version @0.2.0.0@ along with
+-- 'Parameters' data type.
+instance Default (Parameters a) where
+    def = Parameters
+        { paramOutputHandle = const stdout
+        , paramProgName = const ""
+        , paramCommand = const ""
+
+        , paramVersion = const $ Version [] []
+        , paramVersionInfoFooter = const []
+
+        , paramUsage = const []
+        , paramHelpInfoFooter = const []
+        , paramIncludeVersionInfoInHelpInfo = const True
+        , paramIncludeVersionFooterInHelpInfo = const False
+        }
 
 -- | Print usage information in common format:
 --
 -- > <version-info>
+-- > <eol>
 -- > <eol>
 -- > "Usage:" <eol>
 -- > <eol>
 -- > (<space>{4} <program-name> <space> <usage> <eol>)*
 -- > <help>
 -- > (<footer-line> <eol>)*
+--
+-- Introduced in version @0.1.2.0@. Type signature changed in version
+-- @0.2.0.0@.
 printHelp
-    :: String
-    -- ^ Program name.
-    -> Version
-    -- ^ Program version.
-    -> Handle
-    -- ^ 'Handle' to which help information is printed.
-    -> [String]
-    -- ^ Usage information.
-    -> (String -> String)
-    -- ^ Construct help information, parametrized with program name.
-    -> [String]
-    -- ^ Footer lines. Printed right after help information.
+    :: Parameters c
+    -> c
+    -> (String -> String -> String)
+    -- ^ Construct help information, parametrized with program name and
+    -- command.
     -> IO ()
-printHelp progName version h usage mkHelp footer = do
-    printVersion progName [] version h False
-    hPutStr h $ unlines ["", "Usage:", ""]
-    hPutStrLn h . unlines $ map (("   " ++ progName ++ " ") ++) usage
-    hPutStr h $ mkHelp progName
-    hPutStr h $ unlines footer
+printHelp params cfg mkHelp = do
+    when (get paramIncludeVersionInfoInHelpInfo)
+        $ printVersion params' cfg False
+    hPutStr handle . unlines $ emptyLinesIfPrintingVersionInfo ["Usage:", ""]
+    hPutStrLn handle . unlines $ map (("  " ++ command ++ " ") ++) usage
+    hPutStr handle $ mkHelp progName command
+    unless (null footer)
+        . hPutStr handle . unlines $ "" : footer
+  where
+    get f = f params cfg
+    progName = get paramProgName
+    command = get paramCommand
+    handle = get paramOutputHandle
+    usage = get paramUsage
+    footer = get paramHelpInfoFooter
+    params' = params
+        { paramVersionInfoFooter = if get paramIncludeVersionFooterInHelpInfo
+            then paramVersionInfoFooter params
+            else const []
+        }
+    emptyLinesIfPrintingVersionInfo = if get paramIncludeVersionInfoInHelpInfo
+        then ("" :) . ("" :)
+        else id
 
 -- | Print version information in common format:
 --
@@ -58,20 +139,40 @@ printHelp progName version h usage mkHelp footer = do
 -- or just:
 --
 -- > <version> <eol>
+--
+-- Introduced in version @0.1.2.0@. Type signature changed in version
+-- @0.2.0.0@.
 printVersion
-    :: String
+    :: Parameters c
     -- ^ Program name.
-    -> [String]
-    -> Version
-    -- ^ Program version.
-    -> Handle
-    -- ^ 'Handle' to which version information is printed.
+    -> c
     -> Bool
     -- ^ If 'False' then program name is printed along with version number.
     -- When 'True' then only version number is printed.
     -> IO ()
-printVersion progName footer version h numericOnly = do
-    hPutStrLn h . unwords $ withProgName [showVersion version]
-    unless numericOnly . hPutStr h $ unlines footer
+printVersion params cfg numericOnly = do
+    hPutStrLn handle . unwords $ withProgName [showVersion version]
+    unless numericOnly . hPutStr handle $ unlines footer
   where
+    get f = f params cfg
+    progName = get paramProgName
+    footer = get paramVersionInfoFooter
+    version = get paramVersion
+    handle = get paramOutputHandle
     withProgName = if numericOnly then id else (progName :)
+
+-- | Print option errors in this format:
+--
+-- > "Error(s) occurred while processing command line options:" <eol>
+-- > (<space>{2} <option-error> <eol>)+
+--
+-- If list of errors is empty, then nothing is printed.
+--
+-- Introduced in version @0.2.0.0@.
+printOptErrors :: Parameters c -> c -> [String] -> IO ()
+printOptErrors params cfg errs
+  | null errs = return ()
+  | otherwise = hPutStrLn handle . unlines
+    $ "Error(s) occurred while processing command line options:"
+    : map ("  " ++) errs
+  where handle = paramOutputHandle params cfg
